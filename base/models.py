bf3353bd897762795ebe2b874d021e9bcb5ed307
nurilloh2004 <nurillohpython@gmail.com>
2023-01-05 16:23:48 +0500

    addings

--- base/models.py
+++ base/models.py
@@ -5,3 +5,9 @@ Database models
 from django.db import models
 
 
+class Setting(models.Model):
+    key = models.CharField(max_length=1255)
+    value = models.TextField()
+
+    def __str__(self) -> str:
+        return self.key
